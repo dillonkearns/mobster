564ee9de9b5e740c3fcd0fbdf40cbe7e01e20e7e
Dillon Kearns <dillon@dillonkearns.com>
2017-03-10 11:16:08 -0800

    Use mobsterOperationTest DSL for bench test cases

--- tests/MobsterOperationTests.elm
+++ tests/MobsterOperationTests.elm
@@ -8,44 +8,35 @@ import MobsterOperation exposing (MobsterOperation(..), updateMoblist)
 
 all : Test
 all =
-    describe "mobster operation" [ removeTests ]
+    describe "mobster operation" [ benchCases ]
 
 
-removeTests : Test
-removeTests =
-    describe "remove"
-        [ test "list with single item" <|
-            \() ->
-                { empty | mobsters = [ "only item" ] }
-                    |> updateMoblist (Bench 0)
-                    |> Expect.equal
-                        { empty | inactiveMobsters = [ "only item" ], nextDriver = 0 }
-        , test "with multiple items" <|
-            \() ->
-                { empty | mobsters = [ "first", "second" ] }
-                    |> updateMoblist (Bench 0)
-                    |> Expect.equal
-                        { empty | mobsters = [ "second" ], inactiveMobsters = [ "first" ], nextDriver = 0 }
-        , test "driver doesn't change when navigator is removed" <|
-            \() ->
-                { empty | mobsters = [ "Kirk", "Spock", "McCoy" ] }
-                    |> updateMoblist (Bench 1)
-                    |> Expect.equal
-                        { empty | mobsters = [ "Kirk", "McCoy" ], inactiveMobsters = [ "Spock" ], nextDriver = 0 }
-        , test "wraps around list for next driver when nextDriver is removed and was at end of list" <|
-            \() ->
-                { empty | mobsters = [ "Kirk", "Spock", "McCoy" ], nextDriver = 2 }
-                    |> updateMoblist (Bench 2)
-                    |> Expect.equal
-                        { empty | mobsters = [ "Kirk", "Spock" ], inactiveMobsters = [ "McCoy" ], nextDriver = 0 }
+benchCases : Test
+benchCases =
+    describe "bench"
+        [ mobsterOperationTest "list with single item"
+            { empty | mobsters = [ "only item" ] }
+            (Bench 0)
+            { empty | inactiveMobsters = [ "only item" ], nextDriver = 0 }
+        , mobsterOperationTest "with multiple items"
+            { empty | mobsters = [ "first", "second" ] }
+            (Bench 0)
+            { empty | mobsters = [ "second" ], inactiveMobsters = [ "first" ], nextDriver = 0 }
+        , mobsterOperationTest "driver doesn't change when navigator is removed"
+            { empty | mobsters = [ "Kirk", "Spock", "McCoy" ] }
+            (Bench 1)
+            { empty | mobsters = [ "Kirk", "McCoy" ], inactiveMobsters = [ "Spock" ], nextDriver = 0 }
+        , mobsterOperationTest "wraps around list for next driver when nextDriver is removed and was at end of list"
+            { empty | mobsters = [ "Kirk", "Spock", "McCoy" ], nextDriver = 2 }
+            (Bench 2)
+            { empty | mobsters = [ "Kirk", "Spock" ], inactiveMobsters = [ "McCoy" ], nextDriver = 0 }
         ]
 
 
-mobsterOperationTest : MobsterData -> MobsterData -> String -> MobsterOperation -> Test
-mobsterOperationTest startList expectedResult description operation =
+mobsterOperationTest : String -> MobsterData -> MobsterOperation -> MobsterData -> Test
+mobsterOperationTest description startList operation expectedResult =
     test description <|
         \() ->
             startList
                 |> updateMoblist operation
-                |> Expect.equal
-                    expectedResult
+                |> Expect.equal expectedResult
