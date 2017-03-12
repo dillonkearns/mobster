b0a0d7fe0eb91085da58926f3f2982d7c1dd1b57
Dillon Kearns <dillon@dillonkearns.com>
2017-03-11 17:45:07 -0800

    Rename testOperation helper function

--- tests/MobsterOperationTests.elm
+++ tests/MobsterOperationTests.elm
@@ -14,19 +14,19 @@ all =
 benchCases : Test
 benchCases =
     describe "bench"
-        [ mobsterOperationTest "list with single item"
+        [ testOperation "list with single item"
             { empty | mobsters = [ "only item" ] }
             (Bench 0)
             { empty | inactiveMobsters = [ "only item" ], nextDriver = 0 }
-        , mobsterOperationTest "with multiple items"
+        , testOperation "with multiple items"
             { empty | mobsters = [ "first", "second" ] }
             (Bench 0)
             { empty | mobsters = [ "second" ], inactiveMobsters = [ "first" ], nextDriver = 0 }
-        , mobsterOperationTest "driver doesn't change when navigator is removed"
+        , testOperation "driver doesn't change when navigator is removed"
             { empty | mobsters = [ "Kirk", "Spock", "McCoy" ] }
             (Bench 1)
             { empty | mobsters = [ "Kirk", "McCoy" ], inactiveMobsters = [ "Spock" ], nextDriver = 0 }
-        , mobsterOperationTest "wraps around list for next driver when nextDriver is removed and was at end of list"
+        , testOperation "wraps around list for next driver when nextDriver is removed and was at end of list"
             { empty | mobsters = [ "Kirk", "Spock", "McCoy" ], nextDriver = 2 }
             (Bench 2)
             { empty | mobsters = [ "Kirk", "Spock" ], inactiveMobsters = [ "McCoy" ], nextDriver = 0 }
@@ -37,21 +37,21 @@ benchCases2 : Test
 benchCases2 =
     describe "move"
         [ describe "move to inactive"
-            [ mobsterOperationTest "moves a single mobster to an empty bench"
+            [ testOperation "moves a single mobster to an empty bench"
                 { empty | mobsters = [ "Spock" ] }
                 (Bench 0)
                 { empty | inactiveMobsters = [ "Spock" ] }
-            , mobsterOperationsTest "puts mobsters on bench in order they are added"
+            , testOperations "puts mobsters on bench in order they are added"
                 { empty | mobsters = [ "Kirk", "Spock", "McCoy" ] }
                 [ (Bench 1), (Bench 1) ]
                 { empty | mobsters = [ "Kirk" ], inactiveMobsters = [ "Spock", "McCoy" ] }
             ]
         , describe "active"
-            [ mobsterOperationTest "puts mobster back in rotation"
+            [ testOperation "puts mobster back in rotation"
                 { empty | inactiveMobsters = [ "Kirk", "Spock", "McCoy" ] }
                 (RotateIn 2)
                 { empty | inactiveMobsters = [ "Kirk", "Spock" ], mobsters = [ "McCoy" ] }
-            , mobsterOperationTest "adds mobsters back in rotation below the next driver"
+            , testOperation "adds mobsters back in rotation below the next driver"
                 { empty | mobsters = [ "Kirk", "Spock", "McCoy" ], inactiveMobsters = [ "Sulu" ], nextDriver = 1 }
                 (RotateIn 0)
                 { empty | mobsters = [ "Kirk", "Spock", "Sulu", "McCoy" ], nextDriver = 1 }
@@ -62,7 +62,7 @@ benchCases2 =
 removeCases : Test
 removeCases =
     describe "remove"
-        [ mobsterOperationTest "removes an item from bench with no active mobsters"
+        [ testOperation "removes an item from bench with no active mobsters"
             { empty | inactiveMobsters = [ "Kirk", "Spock", "McCoy" ] }
             (Remove 1)
             { empty | inactiveMobsters = [ "Kirk", "McCoy" ] }
@@ -75,7 +75,7 @@ rotateCase1 =
         startList =
             { empty | mobsters = [ "Jane Doe", "John Smith" ], nextDriver = 0 }
     in
-        mobsterOperationTest "without wrapping"
+        testOperation "without wrapping"
             startList
             NextTurn
             { startList | nextDriver = 1 }
@@ -87,7 +87,7 @@ rotateCase2 =
         startList =
             { empty | mobsters = [ "Jane Doe", "John Smith" ], nextDriver = 1 }
     in
-        mobsterOperationTest "with wrapping"
+        testOperation "with wrapping"
             startList
             NextTurn
             { startList | nextDriver = 0 }
@@ -101,35 +101,35 @@ rotateCases =
 moveCases : Test
 moveCases =
     describe "move"
-        [ mobsterOperationTest "single item list"
+        [ testOperation "single item list"
             { empty | mobsters = [ "only item" ], nextDriver = 0 }
             (Move 0 0)
             { empty | mobsters = [ "only item" ], nextDriver = 0 }
-        , mobsterOperationTest "index not in list"
+        , testOperation "index not in list"
             { empty | mobsters = [ "a", "b", "d", "c" ], nextDriver = 0 }
             (Move 4 3)
             { empty | mobsters = [ "a", "b", "d", "c" ], nextDriver = 0 }
-        , mobsterOperationTest "multiple items without wrapping"
+        , testOperation "multiple items without wrapping"
             { empty | mobsters = [ "a", "b", "d", "c" ], nextDriver = 0 }
             (Move 3 2)
             { empty | mobsters = [ "a", "b", "c", "d" ], nextDriver = 0 }
-        , mobsterOperationTest "placing it below hovered slot when moving from higher to lower"
+        , testOperation "placing it below hovered slot when moving from higher to lower"
             { empty | mobsters = [ "a", "b", "c" ], nextDriver = 0 }
             (Move 0 1)
             { empty | mobsters = [ "b", "a", "c" ], nextDriver = 0 }
-        , mobsterOperationTest "to specific position one up"
+        , testOperation "to specific position one up"
             { empty | mobsters = [ "a", "b", "c", "d" ], nextDriver = 0 }
             (Move 3 2)
             { empty | mobsters = [ "a", "b", "d", "c" ], nextDriver = 0 }
-        , mobsterOperationTest "to specific position two up"
+        , testOperation "to specific position two up"
             { empty | mobsters = [ "a", "b", "c", "d" ], nextDriver = 0 }
             (Move 3 1)
             { empty | mobsters = [ "a", "d", "b", "c" ], nextDriver = 0 }
-        , mobsterOperationTest "down below the last item in list"
+        , testOperation "down below the last item in list"
             { empty | mobsters = [ "a", "b", "c", "d" ], nextDriver = 0 }
             (Move 0 3)
             { empty | mobsters = [ "b", "c", "d", "a" ], nextDriver = 0 }
-        , mobsterOperationTest "to specific position several slots away"
+        , testOperation "to specific position several slots away"
             { empty | mobsters = [ "a", "b", "c", "d", "e", "f", "g" ], nextDriver = 0 }
             (Move 6 0)
             { empty | mobsters = [ "g", "a", "b", "c", "d", "e", "f" ], nextDriver = 0 }
@@ -139,19 +139,19 @@ moveCases =
 addCases : Test
 addCases =
     describe "add"
-        [ mobsterOperationTest "add to empty"
+        [ testOperation "add to empty"
             Mobster.empty
             (Add "John Doe")
             { empty | mobsters = [ "John Doe" ] }
-        , mobsterOperationsTest "add two things"
+        , testOperations "add two things"
             Mobster.empty
             [ (Add "Jane Doe"), (Add "John Smith") ]
             { empty | mobsters = [ "Jane Doe", "John Smith" ], nextDriver = 0 }
         ]
 
 
-mobsterOperationTest : String -> MobsterData -> MobsterOperation -> MobsterData -> Test
-mobsterOperationTest description startList operation expectedResult =
+testOperation : String -> MobsterData -> MobsterOperation -> MobsterData -> Test
+testOperation description startList operation expectedResult =
     test description <|
         \() ->
             startList
@@ -159,8 +159,8 @@ mobsterOperationTest description startList operation expectedResult =
                 |> Expect.equal expectedResult
 
 
-mobsterOperationsTest : String -> MobsterData -> List MobsterOperation -> MobsterData -> Test
-mobsterOperationsTest description startList operations expectedResult =
+testOperations : String -> MobsterData -> List MobsterOperation -> MobsterData -> Test
+testOperations description startList operations expectedResult =
     test description <|
         \() ->
             List.foldl updateMoblist startList operations
