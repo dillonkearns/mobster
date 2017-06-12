2b5e97e22440a796222c0a1690214a2f1149407d
Dillon Kearns <dillon@dillonkearns.com>
2017-06-12 10:00:51 -0400

    Remove unnecessary bootstrap row wrapper.

--- src/ViewHelpers.elm
+++ src/ViewHelpers.elm
@@ -1,6 +1,5 @@
 module ViewHelpers exposing (..)
 
-import Basics.Extra exposing ((=>))
 import Html exposing (..)
 import Html.Attributes as Attr exposing (href, id, placeholder, src, style, target, title, type_, value)
 import Html.Events exposing (onClick)
@@ -10,16 +9,14 @@ import StylesheetHelper exposing (CssClasses(..), class, classList, id)
 
 blockButton : String -> msg -> String -> String -> Html msg
 blockButton displayText onClickMsg tooltipText buttonId =
-    div [ Attr.class "row", style [ "padding-bottom" => "1.333em" ] ]
-        [ button
-            [ onClick onClickMsg
-            , Attr.class "btn btn-info btn-lg btn-block"
-            , class [ LargeButtonText, BufferTop, TooltipContainer, Title ]
-            , Attr.id buttonId
-            ]
-            [ div [] [ text displayText ]
-            , div [ class [ Tooltip ] ] [ text tooltipText ]
-            ]
+    button
+        [ onClick onClickMsg
+        , Attr.class "btn btn-info btn-lg btn-block"
+        , class [ LargeButtonText, BufferTop, TooltipContainer, Title ]
+        , Attr.id buttonId
+        ]
+        [ div [] [ text displayText ]
+        , div [ class [ Tooltip ] ] [ text tooltipText ]
         ]
 
 
