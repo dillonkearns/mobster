267a0c312be6f2ec84b4d36658b97c92cf33e456
Dillon Kearns <dillon@dillonkearns.com>
2017-07-16 19:15:09 -0400

    Add return to configure view button in beta view.

--- src/View/Navbar.elm
+++ src/View/Navbar.elm
@@ -4,22 +4,38 @@ import Element exposing (..)
 import Element.Attributes exposing (..)
 import Element.Events exposing (onClick, onInput)
 import Setup.Msg as Msg exposing (Msg)
-import Setup.View exposing (ScreenState)
+import Setup.View exposing (ScreenState(Configure))
 import Styles exposing (StyleElement)
 
 
 view : { model | screenState : ScreenState } -> StyleElement
 view { screenState } =
+    let
+        cogButton =
+            Element.when (screenState /= Configure) settingsPageButton
+    in
     row Styles.Navbar
         [ justify, paddingXY 10 10, verticalCenter ]
         [ row Styles.None [ spacing 12 ] [ roseIcon, el Styles.Logo [] (text "Mobster") ]
         , row Styles.None
             [ spacing 20 ]
-            [ Element.image "./assets/invisible.png" Styles.None [ width (px 40), height (px 40) ] Element.empty
+            [ cogButton
+            , Element.image "./assets/invisible.png" Styles.None [ width (px 40), height (px 40) ] Element.empty
             , navButtonView "Hide" Styles.Warning Msg.hide
             , navButtonView "Quit" Styles.Danger Msg.quit
             ]
         ]
+
+
+settingsPageButton : StyleElement
+settingsPageButton =
+    Element.el Styles.StepButton
+        [ paddingXY 16 10
+        , class "fa fa-cog"
+        , verticalCenter
+        , Element.Events.onClick Msg.OpenConfigure
+        ]
+        Element.empty
 
 
 navButtonView : String -> Styles.NavButtonType -> Msg -> StyleElement
