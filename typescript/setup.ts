00b617928a49b1b954e1ef2b6a0e15b927fcdeee
Dillon Kearns <dillon@dillonkearns.com>
2017-07-22 13:27:32 -0700

    Uncomment invisible hover handling code.

--- typescript/setup.ts
+++ typescript/setup.ts
@@ -38,14 +38,14 @@ document.addEventListener('DOMContentLoaded', function(event) {
   ipcRenderer.on('break-done', function(event: any, elapsedSeconds: any) {
     setup.ports.breakDone.send(elapsedSeconds)
   })
-  // $(() => {
-  //   $('.invisible-trigger').hover(
-  //     () => {
-  //       $('body').addClass('transparent')
-  //     },
-  //     () => {
-  //       $('body').removeClass('transparent')
-  //     }
-  //   )
-  // })
+  $(() => {
+    $('.invisible-trigger').hover(
+      () => {
+        $('body').addClass('transparent')
+      },
+      () => {
+        $('body').removeClass('transparent')
+      }
+    )
+  })
 })
