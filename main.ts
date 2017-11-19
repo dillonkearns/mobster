991cd77ab569d7e874e30cbee5c286b5d6354ba4
Dillon Kearns <dillon@dillonkearns.com>
2017-11-19 11:00:11 -0800

    Remove unused line.

--- main.ts
+++ main.ts
@@ -245,15 +245,6 @@ function onReady() {
 app.on('ready', onReady)
 app.on('before-quit', () => analytics.endSession())
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
 app.on('activate', function() {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
