881dcb15b273ac78086e8571a7011870db9e30de
rarbuthnot <rarbuthnot@inwk.com>
2019-10-16 21:00:12 -0500

    Add tips from The Pragmatic Programmer

--- src/Tip/All.elm
+++ src/Tip/All.elm
@@ -3,9 +3,11 @@ module Tip.All exposing (tips)
 import Tip exposing (Tip)
 import Tip.AgileManifesto
 import Tip.MobProgramming
+import Tip.PragmaticProgrammer
 
 
 tips : List Tip
 tips =
     Tip.AgileManifesto.tips
         ++ Tip.MobProgramming.tips
+        ++ Tip.PragmaticProgrammer.tips
